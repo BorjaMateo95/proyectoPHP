3ce823f26f154b6be14a8988498fb3358bc14a7c
borja <orbitbml@gmail.com>
2018-10-23 10:43:21 +0200

    Modelos

---
+++ Modelos/Caja.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Description of Caja
+ *
+ * @author BORJA
+ */
+class Caja {
+    private $id;
+    private $codigo;
+    private $altura;
+    private $anchura;
+    private $profundidad;
+    private $material;
+    private $color;
+    private $contenido;
+    
+    function __construct($codigo, $altura, $anchura, $profundidad, $material, $color, $contenido) {
+        $this->codigo = $codigo;
+        $this->altura = $altura;
+        $this->anchura = $anchura;
+        $this->profundidad = $profundidad;
+        $this->material = $material;
+        $this->color = $color;
+        $this->contenido = $contenido;
+    }
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getCodigo() {
+        return $this->codigo;
+    }
+
+    function getAltura() {
+        return $this->altura;
+    }
+
+    function getAnchura() {
+        return $this->anchura;
+    }
+
+    function getProfundidad() {
+        return $this->profundidad;
+    }
+
+    function getMaterial() {
+        return $this->material;
+    }
+
+    function getColor() {
+        return $this->color;
+    }
+
+    function getContenido() {
+        return $this->contenido;
+    }
+
+    function setCodigo($codigo) {
+        $this->codigo = $codigo;
+    }
+
+    function setAltura($altura) {
+        $this->altura = $altura;
+    }
+
+    function setAnchura($anchura) {
+        $this->anchura = $anchura;
+    }
+
+    function setProfundidad($profundidad) {
+        $this->profundidad = $profundidad;
+    }
+
+    function setMaterial($material) {
+        $this->material = $material;
+    }
+
+    function setColor($color) {
+        $this->color = $color;
+    }
+
+    function setContenido($contenido) {
+        $this->contenido = $contenido;
+    }
+
+
+
+}
---
+++ Modelos/Estanteria.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Description of Estanteria
+ *
+ * @author BORJA
+ */
+class Estanteria {
+    
+    private $id;
+    private $codigo;
+    private $numlejas;
+    private $ocupadas;
+    private $pasillo;
+    private $numero;
+    
+    function __construct($codigo, $numlejas, $ocupadas, $pasillo, $numero) {
+        $this->codigo = $codigo;
+        $this->numlejas = $numlejas;
+        $this->ocupadas = $ocupadas;
+        $this->pasillo = $pasillo;
+        $this->numero = $numero;
+    }
+
+    
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getCodigo() {
+        return $this->codigo;
+    }
+
+    function getNumlejas() {
+        return $this->numlejas;
+    }
+
+    function getOcupadas() {
+        return $this->ocupadas;
+    }
+
+    function getPasillo() {
+        return $this->pasillo;
+    }
+
+    function getNumero() {
+        return $this->numero;
+    }
+
+    function setCodigo($codigo) {
+        $this->codigo = $codigo;
+    }
+
+    function setNumlejas($numlejas) {
+        $this->numlejas = $numlejas;
+    }
+
+    function setOcupadas($ocupadas) {
+        $this->ocupadas = $ocupadas;
+    }
+
+    function setPasillo($pasillo) {
+        $this->pasillo = $pasillo;
+    }
+
+    function setNumero($numero) {
+        $this->numero = $numero;
+    }
+
+
+}
