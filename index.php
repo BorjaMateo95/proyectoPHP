8c6edd15ab131f755b85901d8b0071bce3c2b5f6
borja <orbitbml@gmail.com>
2018-11-02 19:49:47 +0100

    navbar

--- index.php
+++ index.php
@@ -7,17 +7,22 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        
-        <title>Proyecto Cajas</title>
-        
-        <link href="Estilos/Estilo.css" type="text/css" rel="stylesheet">
-        <link href="Estilos/bootstrap.min.css" type="text/css" rel="stylesheet">
-        
 
-        
+        <title>Proyecto Cajas</title>
+
+        <link href="Estilos/Estilo.css" type="text/css" rel="stylesheet">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+              integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+
     </head>
     <body>
-        
+        <!--
         <nav>
             <ul id="button">
                 <li><a href="Vistas/VistaAltaEstanteria.php">Alta Estanteria</a></li>
@@ -26,10 +31,49 @@ and open the template in the editor.
             </ul>
         </nav>
         
-        
+        -->
+
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+            <div class="container">
+                <a class="navbar-brand" href="./"><b>Almacen Borja</b></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <ul class="navbar-nav mr-auto">
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">INICIO <span class="sr-only">(current)</span></a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">BLOG</a>
+                        </li>
+
+                    </ul>
+
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Buscar ..." aria-label="Buscar ...">
+                        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">BUSCAR</button>
+                    </form>
+
+                    <a class="btn btn-outline-success d-lg-inline-block mb-3 mb-md-0 ml-md-3" href="#">REGISTRATE</a>
+                    <a class="btn btn-outline-info d-lg-inline-block mb-3 mb-md-0 ml-md-3" href="#">INGRESAR</a>
+
+                </div>
+
+            </div>
+
+        </nav>
+
         <?php
         // put your code here
         ?>
-        
+
+
     </body>
 </html>
